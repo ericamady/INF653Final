cc7522576bf614c2213d917158d240981fc03731
Erica Madison-Youman <ermadisonyouman@mail.fhsu.edu>
2025-05-07 22:39:10 -0700

    updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://dazzling-snickerdoodle-777101.netlify.app/',
+    'https://dazzling-snickerdoodle-777101.netlify.app',
     'https://likeable-maple-timpani.glitch.me',
     'http://127.0.0.1:5500',
     'http://localhost:3500'
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@
 
 const whitelist = [
     'https://www.google.com', 
-    'http:127.0.0.1:5500', 
+    'http://127.0.0.1:5500', 
     'http://localhost:3500'
 ];
 
